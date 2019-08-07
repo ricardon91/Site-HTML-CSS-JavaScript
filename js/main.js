a99f859398446cebf74ccb2b9729bcecf70b8cb1
ricardon91 <ricardo.snoronha@gmail.com>
2019-08-07 18:14:03 -0300

    versao 0.2

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*cod pego no site slider para criação dos slides*/
+$(".slider-principal").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+
+});
